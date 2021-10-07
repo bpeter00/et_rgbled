b7462f611193173848d0202e7a15ed39ea7c9238
Peter <tanulo@trefortszki.hu>
2021-10-07 12:43:13 +0200

    rgbled teszt

--- rgbled/rgbled.ino
+++ rgbled/rgbled.ino
@@ -2,8 +2,23 @@ int r=9;
 int b=10;
 int g=11;
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(r,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(g,OUTPUT);
 
+  digitalWrite(r,1);
+  delay(500);
+  digitalWrite(r,0);
+  digitalWrite(b,1);
+  delay(500);
+  digitalWrite(b,0);
+  digitalWrite(g,1);
+  delay(500);
+  digitalWrite(g,0);
+
+  digitalWrite(b,1);
+  digitalWrite(r,1);
+  digitalWrite(g,1);
 }
 
 void loop() {
